6c0a694221832e1f179e95968c759bf52c961509
SKjustSK <sarthaksinghtariyal@gmail.com>
2024-07-20 23:03:00 +0530

    feat: Added TailwindCSS support

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from 'react'
 
 function App() {
   return (
-    <h1>
-      Hello World
+    <h1 className="text-3xl font-bold underline text-blue-600">
+      Hello world!
     </h1>
   )
 }
 
-export default App
+export default App;
