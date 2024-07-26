19073f2278d0635cb6a90c1845b32feea7ea9742
SKjustSK <sarthaksinghtariyal@gmail.com>
2024-07-26 13:47:30 +0530

    docs: CVholder --> CVHolder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import UserInterface from './components/UserInterface'
-import CVholder from './components/CVholder'
+import CVHolder from './components/CVHolder'
 
 function App() {
   return (
     <div className="relative box-border flex min-h-screen bg-[#131313] font-roboto">
       <UserInterface />
-      <CVholder />
+      <CVHolder />
     </div>
   )
 }
--- src/components/UserInterface.jsx
+++ src/components/UserInterface.jsx
@@ -1,15 +1,16 @@
 import Logo from './Logo'
 import PersonalDetails from './UserInterfaceInputs/PersonalDetails'
+import EducationBackground from './UserInterfaceInputs/EducationBackground'
 
 function UserInterface() {
   return (
     <section className="flex w-[40%] flex-col gap-4 p-24 py-8">
       <Logo />
-      <div className="flex flex-col gap-16">
+      <div className="flex flex-col gap-12">
         {/* TODO: Add Sections for User Details */}
 
         <PersonalDetails title="Personal Details" />
-
+        <EducationBackground title="Education Background" />
 
       </div>
     </section>
